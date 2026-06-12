065cb9b484cecf560dd9c1db37017c370b2c3344
zionmezba <mezbazion@gmail.com>
2026-06-12 22:06:50 +0600

    Add phone field to users table and update seeder with existing user data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Abul Mia',
+            'email' => 'abul@gmail.com',
         ]);
     }
 }
--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -1,11 +1,13 @@
 @extends('index')
 
 @section('section-container')
+    <div style="padding:10px">
 
-<h1>
-    {{ $post?->title }}
-</h1>
-<p>
-    {{ $post?->content }}
-</p>
+        <h1>
+            {{ $post?->title }}
+        </h1>
+        <p>
+            {{ $post?->content }}
+        </p>
+    </div>
 @endsection
