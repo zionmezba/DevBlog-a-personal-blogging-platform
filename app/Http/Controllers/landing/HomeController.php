64ee68357843f94e877f2068e71e33a7ba0e1cb6
zionmezba <mezbazion@gmail.com>
2026-06-12 22:19:13 +0600

    Refactor HomeController and landing view structure

--- app/Http/Controllers/landing/HomeController.php
+++ app/Http/Controllers/landing/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 
 // php artisan make:controller HomeController
 class HomeController extends Controller
@@ -10,9 +11,10 @@ class HomeController extends Controller
     public function home()
     {
         $post = Post::first();
+        $user = User::first();
         if ($post->user_id != 1) {
             $post = null;
         }
-        return view('landing.landing', compact('post'));
+        return view('pages.landing.landing', compact('post', 'user'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\landing\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home']);
